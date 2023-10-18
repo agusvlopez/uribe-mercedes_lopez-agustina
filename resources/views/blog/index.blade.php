485361d1afb8870e2388391aac3709ff84e60a30
MerUribe <meruribe@gmail.com>
2023-10-17 21:53:50 -0300

    Se agregó contenido a Blog

--- resources/views/blog/index.blade.php
+++ resources/views/blog/index.blade.php
@@ -4,6 +4,15 @@
 
 @section('content')
         <h1>Blog</h1>
+        <div class="row">
+
+            <div class="col-12 col-md-6 p-4 fs-5">
+                <h2>Blog de Salud y Bienestar</h2>
+
+
+
+            </div>
+        </div>
 
 
 
