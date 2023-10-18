0ea1588ea53f3c70c785ab4ceb1ee9f895e17494
MerUribe <meruribe@gmail.com>
2023-10-17 21:39:32 -0300

    Se insertó contenido en sección about

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -4,5 +4,23 @@
 
 @section('content')
         <h1>Sobre mi</h1>
+        <section>
+            <div class="col-12 col-md-6 p-4 fs-5">
+                <h2>¡Hola!</h2>
+                    <h3>Soy Mercedes Lopez</h3>
+                    <p>Hace más de 10 años que me encuentro involucrada en el campo de la Nutrición, y hace 5 años que trabajo activamente como Nutricionista Matriculada.</p>
+
+                    <p>Soy una persona sumamente empática, que sabe escuchar, con una mirada particularmente reflexiva.</p>
+
+                    <p>Como profesional presento un enfoque orientado a la resolución de problemas, a través del compromiso y la experiencia.</p>
+
+                    <p>En mis sesiones cada encuentro es único. Buscamos otras miradas del problema a tratar.</p>
+
+                    <h3>Me hace muy feliz, y disfruto mucho conectar con mis pacientes!</h3>
+            </div>
+            <div class="col-12 col-md-6">
+                <img class="img-fluid d-block rounded-3 mt-5" src="" alt="Foto">
+            </div>
+        </section>
 
 @endsection
