47c4cf4a505951d4ed0e9a089f0d831c64dcc2ab
agusvlopez <agusvlopez@gmail.com>
2024-01-03 00:59:52 -0300

    Se editó datos.txt y estilo del blog

--- resources/views/blog/clasification/view.blade.php
+++ resources/views/blog/clasification/view.blade.php
@@ -4,50 +4,58 @@
 
 @section('content')
 
-<div class="container mx-auto">
+    <div class="container mx-auto">
+        <div class="row">
+            <div class="p-4 fs-5">
+                <h1 class="text-center">Clasificación: {{ $clasification->name }}</h1>
 
-    <h1 class="text-center">Clasificación: {{$clasification->name}}</h1>
+                <div class="rounded p-4">
+                    @php
+                        $entradas_count = 0;
+                    @endphp
 
-    <div class="rounded p-4">
-        @php
-            $entradas_count = 0;
-        @endphp
-
-        @foreach ($entradas_blog as $entrada_blog)
-        @if ($entrada_blog->clasification_id == $clasification->clasification_id)
-
-        <div>
-            <div class="row g-0 justify-content-center">
-                <div class="card mb-4 shadow " style="max-width: 740px; border:none;">
-                    <div class="row g-0">
-                        <div class="col-md-4">
-                            <img src="{{ asset('storage/' . $entrada_blog->cover)}}" class="img-fluid rounded-start" alt="{{ $entrada_blog->cover_description }}">
-                        </div>
-                        <div class="col-md-8">
-                            <div class="card-body">
-                                <h3 class="card-title border-bottom pb-2">{{$entrada_blog->title}}</h3>
-                                <p class="card-text">{{$entrada_blog->recortar_descripcion()}}</p>
-                                <p><small class="bg-verde badge rounded-pill text-light">{{$entrada_blog->clasification->name}}</small></p>
-                                <p><small>Publicado el {{$entrada_blog->updated_at}}</small></p>
+                    @foreach ($entradas_blog as $entrada_blog)
+                        @if ($entrada_blog->clasification_id == $clasification->clasification_id)
+                            <div>
+                                <div class="row g-0 justify-content-center">
+                                    <div class="card mb-4 shadow " style="max-width: 740px; border:none;">
+                                        <div class="row g-0">
+                                            <div class="col-md-4">
+                                                <img src="{{ asset('storage/' . $entrada_blog->cover) }}"
+                                                    class="img-fluid rounded-start"
+                                                    alt="{{ $entrada_blog->cover_description }}">
+                                            </div>
+                                            <div class="col-md-8">
+                                                <div class="card-body">
+                                                    <h3 class="card-title border-bottom pb-2">{{ $entrada_blog->title }}
+                                                    </h3>
+                                                    <p class="card-text">{{ $entrada_blog->recortar_descripcion() }}</p>
+                                                    <p><small
+                                                            class="bg-verde badge rounded-pill text-light">{{ $entrada_blog->clasification->name }}</small>
+                                                    </p>
+                                                    <p><small>Publicado el {{ $entrada_blog->updated_at }}</small></p>
+                                                </div>
+                                            </div>
+                                            <div class="d-flex justify-content-end pb-3">
+                                                <a href="{{ route('blog.view', ['id' => $entrada_blog->blog_id]) }}"
+                                                    class="link me-4">Seguir leyendo »</a>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                                @php
+                                    $entradas_count++;
+                                @endphp
                             </div>
+                        @endif
+                    @endforeach
+                    @if ($entradas_count === 0)
+                        <div>
+                            <p class="text-center">No existe ninguna entrada con esa clasificación.</p>
                         </div>
-                        <div class="d-flex justify-content-end pb-3">
-                            <a href="{{route('blog.view', ['id' => $entrada_blog->blog_id]   )}}" class="link me-4">Seguir leyendo »</a>
-                        </div>
-                    </div>
+                    @endif
                 </div>
             </div>
-            @php
-                $entradas_count++;
-            @endphp
         </div>
-        @endif
-        @endforeach
-        @if ($entradas_count === 0)
-        <div>
-            <p class="text-center">No existe ninguna entrada con esa clasificación.</p>
-        </div>
-        @endif
     </div>
-</div>
 @endsection
